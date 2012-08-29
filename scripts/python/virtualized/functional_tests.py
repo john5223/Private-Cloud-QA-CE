19667fbf9574ea9f69049ade8bddb325a8b9f950
Jacob Wagner <jacob.wagner@RACKSPACE.com>
2012-08-29 11:45:49 -0500

    showing john cool things

--- scripts/python/virtualized/functional_tests.py
+++ scripts/python/virtualized/functional_tests.py
@@ -1,3 +1,4 @@
 print "!!## -- Starting Virtualized Functional Tests -- ##!!"
 print "!!## -- HELLO BUD -- ##!!"
+print "!!## -- HELLO JOHN -- ##!!"
 print "!!## -- Finished Virtualized Functional Tests -- ##!!"
