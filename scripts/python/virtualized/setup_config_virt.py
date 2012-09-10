2590515d2b6535ab57753c02ba0e9615882460e0
solo4572 <jacob.wagner@rackspace.com>
2012-09-10 14:05:35 -0500

    testing directory stuff

--- scripts/python/virtualized/setup_config_virt.py
+++ scripts/python/virtualized/setup_config_virt.py
@@ -3,7 +3,7 @@ import os.path
 import subprocess
 
 # Change to the workspace directory
-os.chdir('/var/log/jenkins/workspace')
+os.chdir('/var/lib/jenkins/workspace')
 
 # Run git command to print current commit hash
 subprocess.call(['git', 'log', '-1'])
