d6eba95627a7bdf039aa7eddbe0c8bd1cc4515ff
solo4572 <jacob.wagner@rackspace.com>
2012-09-18 17:26:29 -0500

    sanity commit, partly finished the CloudServer class

---
+++ classes/account/Account.py
@@ -0,0 +1,14 @@
+## This class holds account information 
+class Account():
+	## initializer for account class
+	def __init__(self, username, apikey):
+		self.username = username
+		self.apikey = apikey
+
+	## returns information about the account
+	def info():
+		return (self.username, self.apikey)
+	
+	## print information about the account
+	def __str__():
+		print "Username: %s, apikey: %s" % (self.username, self.apikey)
